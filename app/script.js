01a49b9aa9373e6b913e55449f311371aff52ef6
Snippy <148686382+Snipppppy@users.noreply.github.com>
2024-12-03 17:47:04 +0100

    Fix Timestamp on Events Starting @ xx:05

--- app/script.js
+++ app/script.js
@@ -277,7 +277,7 @@ function getLocalTime(uctZeroStartTimeAsString) {
 
 function getTimeAsStr(time){
     let hours = String(time.getHours()).padStart(2, " ")
-    let min = String(time.getMinutes()).padEnd(2, "0")
+    let min = String(time.getMinutes()).padStart(2, "0")
     return `${hours}:${min}`
 }
 
